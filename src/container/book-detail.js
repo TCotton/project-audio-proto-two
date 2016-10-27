4e93e08adedc2375dce1c0fb340ace14edaf5a70
Andy Walpole <andywalpole@Andys-iMac.local>
2016-10-27 14:47:08 +0100

    added default status for BookDetail component

--- src/container/book-detail.js
+++ src/container/book-detail.js
@@ -1,11 +1,32 @@
 import React, {Component} from 'react';
+import { connect } from 'react-redux';
 
-export default class BookDetail extends Component {
+class BookDetail extends Component {
 
 	render() {
+
+		if(!this.props.book) {
+			return <div>Select a book to get started</div>;
+		}
+
 		return (
-			<div>Book Detail!</div>
-		)
+			<div>
+				<h3>Details for:</h3>
+				<p>{this.props.book.title}</p>
+			</div>
+		);
 	}
 
 }
+
+
+function mapStateToProps(state) {
+	// Whatever is returned will show up as props inside of BookList
+
+	return {
+		book: state.activeBook
+	};
+
+}
+
+export default connect(mapStateToProps)(BookDetail);
