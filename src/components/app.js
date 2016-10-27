8475419ef419641b69587f56597d5af2a7776d5c
Andy Walpole <andywalpole@Andys-iMac.local>
2016-10-27 14:36:06 +0100

    added BookDetail dumb component

--- src/components/app.js
+++ src/components/app.js
@@ -1,12 +1,17 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+
 import BookList from '../container/book-list';
+import BookDetail from '../container/book-detail';
 
 export default class App extends Component {
+
   render() {
     return (
       <div>
         <BookList />
+        <BookDetail />
       </div>
-    );
+    )
   }
+
 }
